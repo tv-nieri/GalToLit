d911bbc246f728277abf63740203b4cbc058b7ec
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-04-14 18:51:19 -0300

    first example

---
+++ src/GalToLit.java
@@ -0,0 +1,15 @@
+/*
+    Try This 1-1
+    This program converts gallons to liters.
+*/
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
